1475e4afa113607bcf33e8013e22027867723a9f
Gabriel <gsechaus@gmail.com>
2023-12-11 00:48:57 -0300

    update plugins

--- lua/someone/plugins.lua
+++ lua/someone/plugins.lua
@@ -6,19 +6,45 @@ lvim.plugins = {
 	{
 		"iamcco/markdown-preview.nvim",
 		build = "cd app && npm install",
-		init = function()
+		ft = "markdown",
+		config = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 			vim.g.mkdp_port = "8085"
 			vim.g.mkdp_theme = "dark"
 			vim.g.mkdp_page_title = "${name} - Markdown Preview"
 		end,
-		ft = { "markdown" },
 	},
 	"roobert/tailwindcss-colorizer-cmp.nvim",
 	"NvChad/nvim-colorizer.lua",
 	"mzlogin/vim-markdown-toc",
 	"ThePrimeagen/vim-be-good",
-	"folke/tokyonight.nvim",
-	"marko-cerovac/material.nvim",
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				on_highlights = function(hl, colors)
+					-- hl.LineNr = {
+					-- 	fg = colors.orange,
+					-- }
+					hl.CursorLineNr = {
+						fg = colors.green,
+					}
+				end,
+			})
+		end,
+	},
 	"petertriho/nvim-scrollbar",
+	"rmagatti/alternate-toggler",
 }
+
+table.insert(lvim.plugins, {
+	"zbirenbaum/copilot-cmp",
+	event = "InsertEnter",
+	dependencies = { "zbirenbaum/copilot.lua" },
+	config = function()
+		vim.defer_fn(function()
+			require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+			require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+		end, 100)
+	end,
+})
